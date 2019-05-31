7a1be6c82fc58a37fa14c8166b6e9cb204088339
realkyr <60070075@kmitl.ac.th>
2019-05-31 17:19:22 +0700

    :lipstick: UI Update

--- src/components/Dashboard/Searchbar.vue
+++ src/components/Dashboard/Searchbar.vue
@@ -1,30 +1,31 @@
 <template>
-  <!-- <vs-row class="searchbar"> -->
-  <div class="searchbar row">
-    <!-- <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-xs="2" vs-sm="1" vs-lg="1"> -->
-    <div class="col-2 mt-3 d-flex justify-content-center xs-2 sm-1 lg-1">
-      <img id="logo" src="../../assets/logo/logo.png">
-    <!-- </vs-col> -->
-    </div>
-    <!-- <vs-col vs-w="5" vs-type="flex" vs-align="center"> -->
-    <div class="col-5 mt-2  d-flex align-items-center">
-      <div class="search-input">
-        <input class="thai" type="text" placeholder="ค้นหารถ, นักเรียน">
-        <i class="fas fa-search" aria-hidden="true"></i>
+  <!-- <div class="container-fluid"> -->
+    <div class="searchbar row">
+      <div class="col-2 mt-3 d-flex justify-content-center col-xs-2 col-sm-1 col-lg-1">
+        <img id="logo" src="../../assets/logo/logo.png">
+      <!-- </vs-col> -->
       </div>
-    <!-- </vs-col> -->
-    </div>
-    <!-- <vs-col vs-offset="3" vs-w="2" vs-type="flex" vs-justify="flex-end" vs-align="center"> -->
-    <div class="col mt-3 d-flex justify-content-end">
-      <div class="avatar">
-        <span class="dname">{{ displayName }}</span>
-        <button class="btn" href="#"><i class="fas fa-user"></i></button>
+      <!-- <vs-col vs-w="5" vs-type="flex" vs-align="center"> -->
+      <div class="col-5 mt-2  d-flex align-items-center">
+        <div class="search-input">
+          <input class="thai" type="text" placeholder="ค้นหารถ, นักเรียน">
+          <i class="fas fa-search" aria-hidden="true"></i>
+        </div>
+      <!-- </vs-col> -->
       </div>
-    <!-- </vs-col> -->
+      <!-- <vs-col vs-offset="3" vs-w="2" vs-type="flex" vs-justify="flex-end" vs-align="center"> -->
+      <div class="col-2 offset-3 mt-3 d-flex justify-content-end align-items-center">
+        <div class="avatar">
+          <span class="dname">{{ displayName }}</span>
+          <button class="btn fa-lg" href="#">
+            <i class="fas fa-user">
+          </i></button>
+        </div>
+      <!-- </vs-col> -->
+      </div>
+      <!-- <vs-col vs-w="1"></vs-col> -->
     </div>
-    <!-- <vs-col vs-w="1"></vs-col> -->
-  <!-- </vs-row> -->
-  </div>
+  <!-- </div> -->
 </template>
 
 <script>
@@ -35,7 +36,8 @@ import { mapGetters, mapActions } from 'vuex'
 export default {
   data () {
     return {
-      displayName: this.getDisplayName()
+      // displayName: this.getDisplayName()
+      displayName: 'Phuree K.'
     }
   },
   methods: {
@@ -69,6 +71,7 @@ export default {
 
 .btn {
   display: inline-block;
+  line-height: 10px;
   height: 18pt;
   width: 18pt;
   border-radius: 50%;
