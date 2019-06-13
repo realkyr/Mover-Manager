a0e288e8bee5b7e96e00acf0449d5cce85df4d4c
realkyr <60070075@kmitl.ac.th>
2019-06-13 17:47:17 +0700

    :lipstick: finish drivere UI

---
+++ src/views/ManageDriver.vue
@@ -0,0 +1,27 @@
+<template>
+  <div style="display: flex; flex-flow: column; height: 100vh;">
+    <Searchbar/>
+    <Sidebar />
+    <div class="thai inside-container">
+      <span>จัดการคนขับ</span>
+      <Navtab />
+      <DriverPanel />
+    </div>
+  </div>
+</template>
+
+<script>
+import Searchbar from '../components/Dashboard/Searchbar'
+import Sidebar from '../components/Dashboard/Sidebar'
+import Navtab from '../components/ManageBusDriver/Navtab'
+import DriverPanel from '../components/ManageBusDriver/DriverPanel'
+
+export default {
+  components: {
+    Searchbar,
+    Sidebar,
+    Navtab,
+    DriverPanel
+  }
+}
+</script>
