def091693737fb1320954f7d0c588c92bf819cd8
ingchoff <ingkaratt@gmail.com>
2019-08-12 15:17:55 +0700

    💄add privacy policy app page and update terms

---
+++ src/views/PrivacyApp.vue
@@ -0,0 +1,164 @@
+<template>
+  <div class="Home" style="width: 100%;">
+    <div class="container">
+      <div class="row mt-3">
+        <div class="col d-flex justify-content-end align-items-center">
+          <router-link to="/">
+            <img id="logo" src="../assets/logo/logo.png" />
+          </router-link>
+        </div>
+        <div class="col d-flex justify-content-end align-items-end">
+          <select class="custom-select thai" v-model="lang">
+            <!-- <option value="TH">ไทย</option> -->
+            <option value="EN">English</option>
+          </select>
+        </div>
+      </div>
+      <hr />
+      <div class="thai pl-3 pr-2 mb-5 ml-4 mr-4">
+        <div v-if="lang === 'EN'">
+          <h3 class="font-weight-bold">Privacy Policy For Mover Driver App</h3>
+          <p class="pl-2 pr-2 pl-sm-5 pr-sm-5">
+            &emsp;&emsp;Mover Developer Inc. built the Mover Driver app as a free app. This SERVICE is provided by Mover Developer Inc.
+            at no cost and is intended for use as is. This page is used to inform website visitors regarding our policies with the collection,
+            use, and disclosure of Personal Information if anyone decided to use our Service.
+            If you choose to use our Service, then you agree to the collection and use of information in relation with this policy.
+            The Personal Information that we collect are used for providing and improving the Service. we will not use or share your information
+            with anyone except as described in this Privacy Policy.
+            The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which is accessible at Mover Driver,
+            unless otherwise defined in this Privacy Policy.
+          </p>
+        </div>
+        <div v-if="lang === 'EN'">
+          <h5 class="mt-4 font-italic">Information Collection and Use</h5>
+          <p class="pl-2 pr-2 pl-sm-5 pr-sm-5">
+            &emsp;&emsp;For a better experience while using our Service, we may require you to provide us with certain personally
+            identifiable information including users name, address, location, camera and phone number. The information that we request is
+            will be retained by us and used as described in this privacy policy. <br />
+            The app does use third party services that may collect information used to identify you.
+          </p>
+        </div>
+        <div v-if="lang === 'EN'">
+          <h5 class="mt-4 font-italic">Log Data</h5>
+          <p class="pl-2 pr-2 pl-sm-5 pr-sm-5">
+            &emsp;&emsp;We want to inform you that whenever you use our Service, in case of an error in the app we collect data and
+            information (through third party products) on your phone called Log Data. This Log Data may include information such as
+            your devices’s Internet Protocol (“IP”) address, device name, operating system version, configuration of the app when
+            utilising our Service, the time and date of your use of the Service, and other statistics.
+          </p>
+        </div>
+        <div v-if="lang === 'EN'">
+          <h5 class="mt-4 font-italic">Cookies</h5>
+          <p class="pl-2 pr-2 pl-sm-5 pr-sm-5">
+            &emsp;&emsp;Cookies are files with small amount of data that is commonly used an anonymous unique identifier.
+            These are sent to your browser from the website that you visit and are stored on your devices’s internal memory. <br />
+            This Services does not uses these “cookies” explicitly. However, the app may use third party code and libraries that use
+            “cookies” to collection information and to improve their services. You have the option to either accept or refuse these cookies,
+            and know when a cookie is being sent to your device. If you choose to refuse our cookies, you may not be able to use some portions of this Service.
+          </p>
+        </div>
+        <div v-if="lang === 'EN'">
+          <h5 class="mt-4 font-italic">Service Providers</h5>
+          <div class="pl-2 pr-2 pl-sm-5 pr-sm-5">
+            We may employ third-party companies and individuals due to the following reasons:
+            <ul class="pl-2 pr-2 pl-sm-5 pr-sm-5">
+              <li>
+                To facilitate our Service;
+              </li>
+              <li>
+                To provide the Service on our behalf;
+              </li>
+              <li>
+                To perform Service-related services; or
+              </li>
+              <li>
+                To assist us in analyzing how our Service is used.
+              </li>
+            </ul>
+            We want to inform users of this Service that these third parties have access to
+            your Personal Information. The reason is to perform the tasks assigned to them on our behalf.
+            However, they are obligated not to disclose or use the information for any other purpose.
+          </div>
+        </div>
+        <div v-if="lang === 'EN'">
+          <h5 class="mt-4 font-italic">Security</h5>
+          <p class="pl-2 pr-2 pl-sm-5 pr-sm-5">
+            &emsp;&emsp;We value your trust in providing us your Personal Information,
+            thus we are striving to use commercially acceptable means of protecting it. But remember that
+            no method of transmission over the internet, or method of electronic storage is 100% secure and reliable,
+            and we cannot guarantee its absolute security.
+          </p>
+        </div>
+        <div v-if="lang === 'EN'">
+          <h5 class="mt-4 font-italic">Links to Other Sites</h5>
+          <p class="pl-2 pr-2 pl-sm-5 pr-sm-5">
+            &emsp;&emsp;This Service may contain links to other sites. If you click on a third-party link,
+            you will be directed to that site. Note that these external sites are not operated by us.
+            Therefore, I strongly advise you to review the Privacy Policy of these websites. I have no control over,
+            and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.
+          </p>
+        </div>
+        <div v-if="lang === 'EN'">
+          <h5 class="mt-4 font-italic">Children’s Privacy</h5>
+          <p class="pl-2 pr-2 pl-sm-5 pr-sm-5">
+            &emsp;&emsp;This Services do not address anyone under the age of 15. We do not knowingly collect
+            personal identifiable information from children under 15. In the case We discover that a child under 15
+            has provided us with personal information, we immediately delete this from our servers.
+            If you are a parent or guardian and you are aware that your child has provided us with personal information,
+            please contact us so that we will be able to do necessary actions.
+          </p>
+        </div>
+        <div v-if="lang === 'EN'">
+          <h5 class="mt-4 font-italic">Children’s Privacy</h5>
+          <p class="pl-2 pr-2 pl-sm-5 pr-sm-5">
+            &emsp;&emsp;We may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically
+            for any changes. We will notify you of any changes by posting the new Privacy Policy on this page.
+            These changes are effective immediately, after they are posted on this page.
+          </p>
+        </div>
+        <div v-if="lang === 'EN'">
+          <h5 class="mt-4 font-italic">Contact Us</h5>
+          <p class="pl-2 pr-2 pl-sm-5 pr-sm-5">
+            &emsp;&emsp;If you have any questions or suggestions about our Privacy Policy, do not hesitate to <a href="mailto:yora46@gmail.com">contact us.</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+  data () {
+    return {
+      isAccept: false,
+      lang: 'EN'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.logo-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 10px 0 10px 0;
+  width: 100%;
+}
+.lang-control {
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  margin: 10px 0 10px 0;
+}
+.custom-select {
+  font-size: 10pt;
+  width: 6rem;
+}
+#logo {
+  width: 50px;
+}
+</style>
--- src/views/Terms.vue
+++ src/views/Terms.vue
@@ -25,6 +25,18 @@
             การมอบสิทธิ์การใช้งานข้อมูลส่วนบุคคลตามข้อตกลงที่บริษัทกำหนด รวมถึงการขอสงวนสิทธิ์ในหัวข้อบางประการ
           </p>
         </div>
+        <div v-else>
+          <h3 class="font-weight-bold">Terms and conditions</h3>
+          <p class="pl-2 pr-2 pl-sm-5 pr-sm-5">
+            &emsp;&emsp;The following documents/articles are the descriptions of the characteristics of our product
+            for customers or users to be informed and to make a mutual agreement. Due to the accessibility of various
+            features in the platform owned by the company, customers, buyers, and users need to know
+            the characteristics of the product and understand the diverse consents including,
+            allowing to access information, complying with the rules and regulations when registered to
+            the product, accepting the purchase agreement for business services as specified by the company,
+            and granting the right to use personal information depending on the agreement set by the company.
+          </p>
+        </div>
         <div v-if="lang === 'TH'">
           <h5 class="font-weight-bold">รูปแบบของผลิตภัณฑ์และบริการของทางบริษัท</h5>
           <p class="pl-3 pr-3 pl-sm-5 pr-sm-5">
@@ -32,6 +44,15 @@
             และมีความเกี่ยวข้องกับระบบการทำงานของบุคคลากรหรือตัวนักเรียนนักศึกษา ซึ่งมีขอบข่ายงานปฏิบัติหน้าที่อยู่ในสถานศึกษาต่าง ๆ
             จุดประสงค์เพื่อทำเทคโนโลยีทันสมัย มาพัฒนาปรับปรุงระบบการจัดการของสถานศึกษา อันจะนำมาซึ่งประสิทธิภาพการทำงานที่เพิ่มขึ้น
             ทั้งตัวระบบและโดยรวม
+          </p>
+        </div>
+        <div v-else>
+          <h5 class="font-weight-bold">Form of the product and services of the company</h5>
+          <p class="pl-2 pr-2 pl-sm-5 pr-sm-5">
+            &emsp;&emsp;Under MOVER trademark we provide an educational product which related to school personnel or
+            student operation system which has a functional scope in various educational institutions. Our purpose is
+            to develop new technology to improve the operating system in educational institutions which leads to
+            improvement in the system and overall dimension.
           </p>
         </div>
         <div v-if="lang === 'TH'">
@@ -44,6 +65,17 @@
             </li>
           </ul>
         </div>
+        <div v-else>
+          <h5 class="mt-4 font-italic">Business policy and operation</h5>
+          <ul class="pl-2 pr-2 pl-sm-5 pr-sm-5">
+            <li>The company reserves the right to change the terms and conditions of the service without needing to notify users in advance.</li>
+            <li>
+              The company reserves the right to ban or restrict access to services of any user
+              that do not comply with the regulations or violate the agreement that the service
+              provider has set without needing to inform the reason and the time limit of accessing restriction to the user in advance
+            </li>
+          </ul>
+        </div>
         <div v-if="lang === 'TH'">
           <h5 class="mt-4 font-italic">ข้อตกลงการซื้อบริการ จากผู้ใช้บริการ</h5>
           <ul class="pl-3 pr-3 pl-sm-5 pr-sm-5">
@@ -51,6 +83,17 @@
               ผู้ใช้บริการ จำเป็นต้องทำการซื้อหรือแลกเปลี่ยนอันใดก็ตามอันจะนำมาซึ่งการมอบสิทธ์การเข้าถึงบริการของทางบริษัท
               ตามแต่ที่ผู้ใช้บริการและบริษัทจะทำการตกลงและยอมรับซึ่งกันและกัน ทางบริษัทขอสงวนสิทธิ์ในการกำหนดและเปลี่ยนแปลงข้อตกลงทางธุรกิจ
               การซื้อขาย และการแลกเปลี่ยนโดยไม่จำเป็นต้องแจ้งให้ผู้ใช้บริการได้ทราบล่วงหน้า
+            </li>
+          </ul>
+        </div>
+        <div v-else>
+          <h5 class="mt-4 font-italic">Service purchase agreement</h5>
+          <ul class="pl-2 pr-2 pl-sm-5 pr-sm-5">
+            <li>
+              Users need to purchase or exchange to reserves the right to access to the services providing by
+              the company as a user and the company both agree and accept. The company reserves the right to
+              determine and change the business deal, trading and exchanging without having to notify the user
+              in advance.
             </li>
           </ul>
         </div>
@@ -67,6 +110,22 @@
             <li>
               ผู้ใช้บริการรับทราบว่า การกระทำผิดและละเมิดกฎหมายของทางผู้ใช้บริการนั้น ทางบริษัทไม่มีส่วนเกี่ยวข้องกับการกระทำนั้น ๆ ไม่ว่าด้วยเหตุใดก็ตาม
               ผู้ใช้จะต้องรับผิดชอบแต่เพียงผู้เดียว และจะต้องรับผิดชอบต่อความเสียหายอันเกิดกับบริษัทและผลิตภัณฑ์ของบริษัท
+            </li>
+          </ul>
+        </div>
+        <div v-else>
+          <h5
+            class="mt-4 font-italic"
+          >Terms of service and agreements on account owners and users’ demeanor</h5>
+          <ul class="pl-2 pr-2 pl-sm-5 pr-sm-5">
+            <li>Users agree not to enter or bring false information into the system which affects the overall system operation of Mover’s product.</li>
+            <li>Users agree not to enter or bring illegal information which violates both civil and criminal laws into the product’s system.</li>
+            <li>Users agree not to enter or bring any information including sets of photos, videos, or messages which related or conveyed in an obscene way.</li>
+            <li>Users agree not to enter or bring any information which humiliating person or insulting the monarchy.</li>
+            <li>Users agree not to enter or bring any information which humiliating person or insulting the monarchy.</li>
+            <li>
+              Users acknowledged that the company has no connection with the offense and violation of the law done by the user. For whatever reason,
+              users must be solely responsible and will be responsible for the damaged on the company and the products.
             </li>
           </ul>
         </div>
@@ -88,6 +147,25 @@
             </li>
           </ul>
         </div>
+        <div v-else>
+          <h5 class="mt-4 font-italic">Agreement on users’ data accessing rights</h5>
+          <ul class="pl-2 pr-2 pl-sm-5 pr-sm-5">
+            <li>
+              Users grant the company to access any form of data. The company able to access account information,
+              pictures, videos, messages, identifying and tracking locations, or any information that the user
+              has entered or imported into the platform, both intentionally and unintentionally including, directly
+              and indirectly data importation which done by the user.
+            </li>
+            <li>
+              Users grant the company to access any form of data that users have been entered or imported into relevant platforms,
+              related services, or side products which have been connected to share or transfer data and to do various actions together.
+            </li>
+            <li>
+              The company reserves the right to change the agreement on users’ data accessing rights of the service without needing to
+              notify users in advance and considering that the user has been informed and agreed to the agreement when registered to the service.
+            </li>
+          </ul>
+        </div>
         <div v-if="lang === 'TH'">
           <h5 class="mt-4 font-italic">นโยบายด้านการใช้งานข้อมูลของผู้ใช้บริการ</h5>
           <ul class="pl-3 pr-3 pl-sm-5 pr-sm-5">
@@ -102,6 +180,22 @@
             <li>
               ผู้ใช้บริการรับทราบว่า การกระทำผิดและละเมิดกฎหมายของทางผู้ใช้บริการนั้น ทางบริษัทไม่มีส่วนเกี่ยวข้องกับการกระทำนั้น ๆ ไม่ว่าด้วยเหตุใดก็ตาม
               ผู้ใช้จะต้องรับผิดชอบแต่เพียงผู้เดียว และจะต้องรับผิดชอบต่อความเสียหายอันเกิดกับบริษัทและผลิตภัณฑ์ของบริษัท
+            </li>
+          </ul>
+        </div>
+        <div v-else>
+          <h5 class="mt-4 font-italic">Policy on the usage of users’ information</h5>
+          <ul class="pl-2 pr-2 pl-sm-5 pr-sm-5">
+            <li>The company will not disclose any information of the user to the public on both online and offline platforms without prior approval from the user.</li>
+            <li>The company will not disclose users’ information or allow other companies, services, or platforms that are irrelevant to access or transfer users’ data.</li>
+            <li>
+              The purpose of collecting and using users’ information is to improve the quality of the product and to enhance the quality of the user experience.
+            </li>
+            <li>Users agree not to enter or bring any information which humiliating person or insulting the monarchy.</li>
+            <li>Users agree not to enter or bring any information with the purpose of instigating incitement or intention to cause controversy.</li>
+            <li>
+              Users acknowledged that the company has no connection with the offense and violation of the law done by the user. For whatever reason,
+              users must be solely responsible and will be responsible for the damaged on the company and the products.
             </li>
           </ul>
         </div>
@@ -131,14 +225,51 @@
             </li>
           </ul>
         </div>
+        <div v-else>
+          <h5 class="mt-4 font-italic">Agreement on data usage rights</h5>
+          <ul class="pl-2 pr-2 pl-sm-5 pr-sm-5">
+            <li>
+              Users agree to allow the company to use account information, images, video, messages,
+              location-identifying/tracking, or any form of information that the user has entered or
+              imported into the platform, both intentionally and unintentionally, directly and indirectly,
+              to analyze and to improve the quality of the product.
+            </li>
+            <li>
+              Users agree to allow the company to arrange and analyze any information of the user and
+              allow the company to transfer or share information to relevant systems, to develop the products
+              and services of the company.
+            </li>
+            <li>
+              Users who have entered or imported any form of information into the service
+              system of the company agrees to grant the company the right to transfer, copy,
+              arrange, analyze, change, and delete data depending on the policy of the service provider.
+            </li>
+            <li>
+              Users agree to grant the company the right to publish or transfer users' information related
+              to the system of the product to the system of related products.
+            </li>
+            <li>
+              The company reserves the right to change the agreement on data usage rights of the service
+              without needing to notify users in advance and considering that the user has been informed and
+              agreed to the agreement when registered to the service.
+            </li>
+          </ul>
+        </div>
         <div class="d-flex justify-content-center flex-column">
           <div class="custom-control custom-checkbox mt-3">
             <input id="terms" type="checkbox" class="custom-control-input" v-model="isAccept" />
             <label
+              v-if="lang === 'TH'"
               style="font-size:12pt;"
               class="thai custom-control-label"
               for="terms"
             >ฉันยอมรับข้อตกลงการใช้งาน</label>
+            <label
+              v-else
+              style="font-size:12pt;"
+              class="thai custom-control-label"
+              for="terms"
+            >I accept the terms and conditions.</label>
           </div>
           <div class="btn-regis">
             <button
@@ -148,8 +279,14 @@
               :class="{ 'disabled': !isAccept }"
               @click="register"
               :disabled="!isAccept"
-            >สมัครสมาชิก</button>
-            <button type="button" id="back" class="btn thai" @click="cancel">ยกเลิก</button>
+            >
+              <span v-if="lang === 'TH'">สมัครสมาชิก</span>
+              <span v-else>Sign up</span>
+            </button>
+            <button type="button" id="back" class="btn thai" @click="cancel">
+              <span v-if="lang === 'TH'">ยกเลิก</span>
+              <span v-else>cancel</span>
+            </button>
           </div>
         </div>
       </div>
@@ -166,7 +303,7 @@ export default {
   data () {
     return {
       isAccept: false,
-      lang: 'TH'
+      lang: 'EN'
     }
   },
   methods: {
